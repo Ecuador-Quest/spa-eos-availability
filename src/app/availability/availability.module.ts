5d5a3c5fd362f7b320fe516c949642d64e5abed1
Leonardo Medina <leoz.31@hotmail.com>
2020-03-12 21:08:49 -0500

    pause

--- src/app/availability/availability.module.ts
+++ src/app/availability/availability.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {PerfectScrollbarModule} from 'ngx-perfect-scrollbar';
 
 
 
 @NgModule({
   declarations: [MainComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSidenavModule,
+    PerfectScrollbarModule,
+
   ]
 })
 export class AvailabilityModule { }
--- src/app/availability/main/main.component.ts
+++ src/app/availability/main/main.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+import { Router } from '@angular/router';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnDestroy {
 
-  constructor() { }
+  mobileQuery: MediaQueryList;
+  dir = 'ltr';
+  green: boolean;
+  blue: boolean;
+  dark: boolean;
+  minisidebar: boolean;
+  boxed: boolean;
+  danger: boolean;
+  showHide: boolean;
+  url: string;
+  sidebarOpened;
+  status = false;
 
-  ngOnInit() {
+  public showSearch = false;
+
+  public config: PerfectScrollbarConfigInterface = {};
+  private _mobileQueryListener: () => void;
+
+  clickEvent() {
+    this.status = !this.status;
   }
 
+  constructor(
+    public router: Router,
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher
+  ) {
+    this.mobileQuery = media.matchMedia('(min-width: 768px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
